174353d3fd01d6b204080cab417e61ed829c1768
Jimmy Tan <tanjimmy1129@gmail.com>
2019-05-15 15:01:24 -0400

    made boxes

--- main.js
+++ main.js
@@ -20,8 +20,31 @@ class Message {
 user1 = new User('jimmy@gmail.com', '111111')
 user2 = new User('me@gmail.com', '111111')
 
+let user1form = document.getElementById('user1-form')
+let user2inbox = document.getElementById('user2-inbox')
+window.addEventListener('keydown', function(event) {
+  if(event.which == 'Shift' && event.which == 13){
+    
+  }
+
+  if (event.which == 13) {
+  }
+})
 
 
+function sendMessage() {
+  let text = document.getElementById('msg-content')
+  let p = document.createElement('p')
+  p.innerText = text.value
+  user2inbox.appendChild(p)
+  text.value = ''
+  text.focus()
+}
+
+user1form.addEventListener('submit', function(event) {
+  event.preventDefault()
+  sendMessage()
+})
 // sendMessage(recipient) {
 //   let msg = new Message()
 //   console.log('Message sent: ' + msg.content)
